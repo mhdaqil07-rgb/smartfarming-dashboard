f106b40bbb649ba4209fe70abf4c51d4b1830a37
mhdaqil07-rgb <mhdaqil07@gmail.com>
2026-01-05 16:55:02 +0700

    smartfarm_dashboard.py

--- smartfarm_dashboard.py
+++ smartfarm_dashboard.py
@@ -8,6 +8,9 @@ st.caption("Monitoring Suhu & Kelembaban (ESP32 → Firebase)")
 
 FIREBASE_URL = "https://dhtttt-17fe2-default-rtdb.firebaseio.com/smartfarm/dht11.json"
 
+if st.button("🔄 Refresh Data"):
+    st.rerun()
+
 response = requests.get(FIREBASE_URL, timeout=10)
 data = response.json()
 
@@ -17,17 +20,15 @@ if data:
     with col1:
         st.metric(
             "🌡️ Suhu (°C)",
-            float(data["temperature"]),
-            key=f"suhu_{data['timestamp']}"
+            float(data.get("temperature", 0))
         )
 
     with col2:
         st.metric(
             "💧 Kelembaban (%)",
-            float(data["humidity"]),
-            key=f"hum_{data['timestamp']}"
+            float(data.get("humidity", 0))
         )
 
-    st.caption(f"Last update: {data['timestamp']}")
+    st.success("Data berhasil ditampilkan")
 else:
-    st.warning("Data belum tersedia")
+    st.warning("Data belum tersedia di Firebase")
